245161d474dc23b597b279a4b71b2f9280f72336
Luca Guadagnini <luca.guadagnini@gmail.com>
2020-10-09 01:06:27 +0200

    added a rich-text sample

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+open module io.trydent.snowflake {
+  requires org.fxmisc.richtext;
+  requires javafx.graphics;
+  requires javafx.controls;
+  requires flowless;
+  requires reactfx;
+
+  exports io.trydent;
+}
